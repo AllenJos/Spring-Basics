00796a269241536647dc5d9418d5f730cc80b996
Allen Joseph <allenjos1996@gmail.com>
2022-11-07 14:34:53 +0100

    Multiple DB

--- Spring multiple db/demo-multiple-db/src/main/java/com/example/demomultipledb/PersonDBConfig.java
+++ Spring multiple db/demo-multiple-db/src/main/java/com/example/demomultipledb/PersonDBConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -14,7 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
-@ConfigurationProperties
+@Configuration
 @EnableJpaRepositories(
         basePackages = {"com.example.demomultipledb.persondb"},
         entityManagerFactoryRef = "personEntityManager",
